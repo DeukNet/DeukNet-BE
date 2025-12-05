0300f018f3b3fb03bbb78adbcf2bcc957fd80638
kkm06100 <kkm06101010@gmail.com>
2025-12-05 13:53:43 +0900

    주석 제거

--- deuknet-application/src/main/java/org/example/deuknetapplication/service/file/FileDownloadService.java
+++ deuknet-application/src/main/java/org/example/deuknetapplication/service/file/FileDownloadService.java
@@ -11,12 +11,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 파일 다운로드 Service
- *
+
  * 책임:
  * - 파일 다운로드 요청 처리
  * - 파일 존재 여부 확인
  * - 경로 탐색 공격 방지
- *
+
  * 참고:
  * - MIME 타입 검증은 업로드 시점에 완료됨
  * - 다운로드는 검증된 파일만 제공
@@ -35,7 +35,6 @@ public class FileDownloadService implements DownloadFileUseCase {
         validateFileName(fileName);
 
         try {
-            // 파일 다운로드 (업로드 시점에 검증 완료되었으므로 추가 검증 불필요)
             String contentType = fileStoragePort.getContentType(fileName);
 
             return new FileDownloadResponse(
